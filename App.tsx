b7bac407b621516bc7acc104d61d022f25f8016b
app-c <william@app-com.digital>
2023-05-20 12:59:58 -0300

    add fonts on base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
 import 'react-native-gesture-handler';
+import {
+  useFonts,
+  Mitr_200ExtraLight as Light,
+  Mitr_400Regular as Regular,
+  Mitr_700Bold as Bold,
+} from '@expo-google-fonts/mitr';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Light,
+    Regular,
+    Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View onLayout={onLayoutRootView}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
